d949c5edbda0ee6d8096e0afaaf82aa9b3576466
anggawidiarta <anggawidiarta55@gmail.com>
2023-11-14 23:19:23 +0800

    fixed

--- projects/wikipaedi/script.js
+++ projects/wikipaedi/script.js
@@ -13,8 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   function searchWikipedia(searchTerm) {
-    const url = `https://id.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=500&srsearch=${encodeURI(
-    searchTerm
+    const url = `https://id.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=25&srsearch=${encodeURIComponent(
+      searchTerm
     )}`;
     fetch(url)
       .then((response) => response.json())
@@ -25,15 +25,27 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function displayResults(results) {
+    // Set the inner HTML of the resultsContainer to an empty string. This clears any previous search results.
     resultsContainer.innerHTML = "";
-    resultsCounter.textContent = `Results Count : ${results.length}`;
+
+    // Update the text content of the resultsCounter to display the number of search results returned.
+    resultsCounter.textContent = `Hasil Pencarian : ${results.length}`;
+
+    // Iterate over each result in the results array.
     results.forEach((result) => {
+      // Create a new div element for each result.
       const resultElement = document.createElement("div");
+
+      // Add a class of "result" to the div.
       resultElement.className = "result";
+
+      // Set the inner HTML of the div to include the title, snippet, and a link to the full Wikipedia page.
       resultElement.innerHTML = `
       <h3>${result.title}</h3>
       <p>${result.snippet}</p>
       <a href="https://id.wikipedia.org/?curid=${result.pageid}" target="_blank">Read More</a>`;
+
+      // Append the result div to the resultsContainer.
       resultsContainer.appendChild(resultElement);
     });
   }
