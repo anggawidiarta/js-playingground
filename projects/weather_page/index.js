cfc1e2e7f37e9927a74cc78100dd2e8b486ac6c5
anggawidiarta <anggawidiarta55@gmail.com>
2023-11-20 16:41:32 +0800

    update

--- projects/weather_page/index.js
+++ projects/weather_page/index.js
@@ -23,5 +23,38 @@ search.addEventListener("click", () => {
         error404.classList.add("fadeIn");
         return;
       }
+      error404.style.display = "none";
+      error404.classList.remove("fadeIn");
+      const image = document.querySelector(".weather-box img");
+      const temperature = document.querySelector(".weather-box .temperature");
+      const description = document.querySelector(".weather-box .description");
+      const humidity = document.querySelector(".weather-box .humidity");
+      const wind = document.querySelector(".weather-box .wind span");
+      switch (json.weather[0].main) {
+        case "Clear":
+          image.src = "images/clear.png";
+          break;
+        case "Rain":
+          image.src = "images/rain.png";
+          break;
+        case "Snow":
+          image.src = "images/snow.png";
+          break;
+        case "Clouds":
+          image.src = "images/clouds.png";
+          break;
+        case "Haze":
+          image.src = "images/haze.png";
+          break;
+        default:
+          image.src = "";
+      }
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+      weatherBox.style.display = "";
+      weatherDetails.style.display = "";
+      weatherBox.classList.add("fadeIn");
+      contaier.styke.height = "590px";
     });
 });
