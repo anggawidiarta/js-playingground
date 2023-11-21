1e6f0ee2c274123605d8882b67a42bbe71129d80
anggawidiarta <anggawidiarta55@gmail.com>
2023-11-21 11:38:46 +0800

    new

--- projects/weather_page/script.js
+++ projects/weather_page/script.js
@@ -5,18 +5,25 @@ const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
 
 search.addEventListener("click", async () => {
+  // API Key for OpenWeatherMap
   const APIKey = "dda1c7218df9e346c6f541c49a54fca7";
+
+  // Get the value of the input box
   const city = document.querySelector(".search-box input").value;
 
+  // If input box is empty, return
   if (city === "") return;
 
   try {
+    // Fetch the weather data for the city from OpenWeatherMap
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
     );
 
+    // Convert the response to JSON
     const json = await response.json();
 
+    // If the city is not found, display error and return
     if (json.cod === "404") {
       container.style.height = "400px";
       weatherBox.style.display = "none";
@@ -26,15 +33,18 @@ search.addEventListener("click", async () => {
       return;
     }
 
+    // If the city is found, hide the error
     error404.style.display = "none";
     error404.classList.remove("fadeIn");
 
+    // Get elements to display weather data
     const image = document.querySelector(".weather-box img");
     const temperature = document.querySelector(".weather-box .temperature");
     const description = document.querySelector(".weather-box .description");
     const humidity = document.querySelector(".weather-details .humidity span");
     const wind = document.querySelector(".weather-details .wind span");
 
+    // Display the appropriate image for the weather
     switch (json.weather[0].main) {
       case "Clear":
         image.src = "images/clear.png";
@@ -60,17 +70,20 @@ search.addEventListener("click", async () => {
         image.src = "";
     }
 
+    // Display the weather data
     temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
     description.innerHTML = `${json.weather[0].description}`;
     humidity.innerHTML = `${json.main.humidity}%`;
     wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
+    // Show the weather box and details
     weatherBox.style.display = "";
     weatherDetails.style.display = "";
     weatherBox.classList.add("fadeIn");
     weatherDetails.classList.add("fadeIn");
     container.style.height = "590px";
   } catch (error) {
+    // Log any errors
     console.error("Error:", error);
   }
 });
