8b140c6498e849eb0165e5bfdd73bc4f5f58d35c
anggawidiarta <anggawidiarta55@gmail.com>
2023-12-01 23:11:14 +0800

    new

---
+++ challenge/test.js
@@ -0,0 +1,10 @@
+console.log("hi");
+setTimeout(() => {
+  console.log("world");
+}, 0);
+console.log("are");
+setTimeout(() => {
+  console.log("you");
+}, 1000);
+console.log("ready");
+console.log("kids");
