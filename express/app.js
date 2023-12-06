b049aaa4d953681d843ca8f76681a27467261224
anggawidiarta <anggawidiarta55@gmail.com>
2023-12-06 09:47:44 +0800

    new

--- express/app.js
+++ express/app.js
@@ -1,18 +1,39 @@
+
+// Memuat modul express
 const express = require("express");
 
+// Membuat aplikasi express
 const app = express();
 
+// Menentukan port server
 const port = 3000;
 
+// Menangani permintaan GET ke rute root ("/")
 app.get("/", (req, res) => {
+  // Mengirim "Hello World!" sebagai respon
   res.send("Hello World!");
 });
 
-app.get('/about',(req,res)=>{
-    res.send('This Is About')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// Menangani permintaan GET ke rute "/about"
+app.get("/about", (req, res) => {
+  // Mengirim file about.html sebagai respon
+  res.sendFile("./about.html", { root: __dirname });
 });
 
+// Menangani permintaan GET ke rute "/about/product/:id"
+app.get("/about/product/:id", (req, res) => {
+  // Mengirim "Product ID : [id]" sebagai respon, di mana [id] adalah id produk yang diterima dari parameter rute
+  res.send(`Product ID : ${req.params.id}`);
+});
+
+// Menangani permintaan GET ke rute "/about/product/shoes=converse"
+app.get("/about/product/shoes=converse", (req, res) => {
+  // Mengirim "Product Query : [query]" sebagai respon, di mana [query] adalah query yang diterima dari parameter rute
+  res.send(`Product Query : ${req.query.shoes}`);
+});
+
+// Menjalankan server di port yang ditentukan
+app.listen(port, () => {
+  // Menampilkan pesan log ketika server berjalan
+  console.log(`Example app listening on port ${port}`);
+});
