f5e4a178c17586594d9e9b3ba4b65da96f2da2c8
anggawidiarta <anggawidiarta55@gmail.com>
2023-12-20 10:40:57 +0800

    new

--- projects/markdown_blog/routes/articles.js
+++ projects/markdown_blog/routes/articles.js
@@ -1,6 +1,8 @@
+// Importing the 'express' framework to create a router
 const express = require("express");
 const router = express.Router();
-// Importing the Article model
+
+// Importing the Article model from the models directory
 const Article = require("../models/article");
 
 // Route to render the form for creating a new article
@@ -9,15 +11,17 @@ router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
 });
 
+// Route to render the form for editing an existing article
 router.get("/edit/:id", async (req, res) => {
+  // Fetching the article with the specified ID from the database
   const article = await Article.findById(req.params.id);
-  // Rendering the 'new' view and passing a new Article instance as a local variable
+  // Rendering the 'edit' view and passing the fetched article as a local variable
   res.render("articles/edit", { article: article });
 });
 
-// Route to handle the request for a specific article by its ID
+// Route to handle the request for a specific article by its slug
 router.get("/:slug", async (req, res) => {
-  // Fetching the article from the database using the ID from the request parameters
+  // Fetching the article from the database using the slug from the request parameters
   const article = await Article.findOne({ slug: req.params.slug });
   // If no article is found, redirect to the home page
   if (article == null) {
@@ -31,40 +35,48 @@ router.get("/:slug", async (req, res) => {
 router.post(
   "/",
   async (req, res, next) => {
+    // Creating a new Article instance and attaching it to the request object
     req.article = new Article();
     next();
   },
   saveArticleAndRedirect("new")
 );
 
+// Route to handle the form submission for editing an existing article
 router.put(
   "/:id",
   async (req, res, next) => {
+    // Fetching the existing article with the specified ID from the database
     req.article = await Article.findById(req.params.id);
     next();
   },
   saveArticleAndRedirect("edit")
 );
 
+// Route to handle the deletion of an article by its ID
 router.delete("/:id", async (req, res) => {
+  // Deleting the article with the specified ID from the database
   await Article.findByIdAndDelete(req.params.id);
+  // Redirecting to the home page after deletion
   res.redirect("/");
 });
 
+// Function to save an article to the database and redirect based on the provided path
 function saveArticleAndRedirect(path) {
   return async (req, res) => {
     let article = req.article;
+    // Updating the article properties with the submitted form data
     article.title = req.body.title;
     article.description = req.body.description;
     article.markdown = req.body.markdown;
 
     try {
-      // Attempting to save the new article to the database
+      // Attempting to save the updated/new article to the database
       article = await article.save();
-      // If successful, redirect to the page of the newly created article
+      // If successful, redirect to the page of the newly created/edited article
       res.redirect(`/articles/${article.slug}`);
     } catch (e) {
-      // If an error occurs, re-render the form view with the current data
+      // If an error occurs, log it and re-render the form view with the current data
       console.log(e);
       res.render(`articles/${path}`, { article: article });
     }
